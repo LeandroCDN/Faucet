817dca82089936eadee5a9369db70412d5dcbca2
Leandro <leandro@Air-de-Leandro.fibertel.com.ar>
2023-03-23 07:33:56 -0300

    Balance text

--- src/Components/Button/index.js
+++ src/Components/Button/index.js
@@ -1,21 +1,23 @@
 import React, { useState, useEffect } from 'react';
 import './Button.css';
 import { ethers } from 'ethers';
-// import ERC20 from "../../Contracts/erc20.json"
+import ERC20ABI from "../../Contracts/erc20.json"
 import faucet from "../../Contracts/faucet.json"
 
 // Debe estar apuntando a polygon testnet
 const usdMockAddress = "0xD6920eeAF9b9bc7288765F72B4d6Da3e47308464";
 const faucetAddress = "0x20964124c3f870BD95c842836d75E91F5BAFdF7b";
 const faucetABI = faucet;
-
 function Button() {
   let signer = '';
   let faucetContract;
+  let usdMockContract;
   const [isConnected, setIsConnected] = useState(false);
+  const [balance, setBalance] = useState('0');
   
   const initContract = async () => {
     faucetContract = new ethers.Contract(faucetAddress, faucetABI, signer);
+    usdMockContract = new ethers.Contract(usdMockAddress, ERC20ABI, signer);
   }
 
   useEffect(() => {
@@ -32,6 +34,7 @@ function Button() {
         const provider = new ethers.providers.Web3Provider(window.ethereum);
         signer = await provider.getSigner();
         await initContract();
+        await getBalance();
         setIsConnected(true); // Actualiza el estado
 
       } catch (error) {
@@ -54,6 +57,11 @@ function Button() {
         gasLimit: 300000,
       });
         console.log(tx);
+
+        await tx.wait().then(() => {
+          getBalance();
+          console.log(balance);
+        });
       } catch (error) {
         console.error(error);
       }
@@ -62,13 +70,21 @@ function Button() {
     }
   };
 
+  const getBalance = async () => {
+    const balanceOf = (await usdMockContract.balanceOf(signer.getAddress())).toString();
+    setBalance(parseFloat(ethers.utils.formatEther(balanceOf)).toFixed(2));
+
+  }
+
   return (
-    <div className="App">
+    <div className="button">
       <div className='container'>
-        <p></p>
         <button className='myboton' onClick={isConnected ? claim : connectWallet }>
-          <p className='buttontext'>{isConnected ? 'Claim tokens': 'Connect to wallet'}</p> 
+          <p className='buttontext'>{isConnected ? 'Claim 100 usdMock': 'Connect to wallet'}</p> 
         </button>     
+        <div className='textDiv'>
+         <p className='text'>Cantidad de tokens: {balance}</p>
+        </div>
       </div>
     </div>
   );
